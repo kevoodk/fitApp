ffd98a04d58aa3bd319517dd1b391fed4bd03756
kevoodk <kevsmultimedia@gmail.com>
2020-12-10 15:29:44 +0100

    styles

---
+++ resources/views/fitnessplan/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                <form method="POST" action="/fitnessplan/update/{{$fitness['id']}}">
+                @csrf
+
+                        <div class="form-row">
+                            <div class="col">
+                                <label for="fitName">Name of the plan</label>
+                                <input name="name" class="form-control" type="text" id="fitName" value="{{$fitness['name']}}"placeholder="Name of the fitness plan..."/>
+                            </div>
+                            <div class="col">
+                                <label for="fitDay">What day would you start the program</label>
+                               <select value="{{$fitness['workout_day']}}" class="form-control" multiple name="workout_day[]" data-max-options="2">
+                                    <option value="monday">Monday</option>
+                                    <option value="tuesday">Tuesday</option>
+                                    <option value="wednesday">Wednesday</option>
+                                    <option value="thursday">Thursdag</option>
+                                    <option value="friday">Friday</option>
+                                    <option value="saturday">Saturday</option>
+                                    <option value="sunday">Sunday</option>
+                                </select>
+                            </div>
+                        </div>
+
+                            @foreach($excercises as $excercise)
+                                 <div class="form-row">
+                            <div class="col">
+                                <label>{{$excercise['name']}}</label>
+                                <input type="checkbox" class="form-control" name="excercises[]" value="{{$excercise['id']}}"/>
+                            </div>
+                            <div class="col">
+                                <label>Antal reps</label>
+                                <select class="form-control" name="reps" id="reps">
+                                    <option disabled selected value> -- select an option -- </option>
+                                @for ($i = 1; $i < 21; $i++)
+                                    <option value="{{$i}}">{{$i}}</option>
+                                @endfor
+                                </select>
+                            </div>
+                            <div class="col">
+                                <label for="sets">Antal sets</label>
+                                <select class="form-control" name="sets" id="sets">
+                                    <option disabled selected value> -- select an option -- </option>
+                                @for ($i = 1; $i < 21; $i++)
+                                    <option value="{{$i}}">{{$i}}</option>
+                                @endfor
+                                </select>
+                            </div>
+                        </div>
+                            @endforeach
+                        </div>
+
+
+                        <input type="submit" class="btn btn-primary form-control" value="Update"/>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
--- resources/views/fitnessplan/index.blade.php
+++ resources/views/fitnessplan/index.blade.php
@@ -5,12 +5,36 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                @foreach($kevoos as $plan)
-                    <a href="{{url('/fitnessplan/'. $plan['id'])}}">{{$plan['name']}}</a>
-                @endforeach
+                <div class="card-header">My workouts</div>
+                <table class="table table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">Program name</th>
+                            <th scope="col">Weekday(s)</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($fitness as $plan)
+                        <tr>
+                            <td><a href="{{url('/fitnessplan/'. $plan['id'])}}">{{$plan['name']}}</a></td>
+                            <td></td>
+                            <td>  <a href="{{url('/fitnessplan/edit/'. $plan['id'])}}">
+                            <i class="fa fa-edit  fa-lg"></i>
+                            </a></td>
+                            <td><form action="{{url('/fitnessplan/delete/'. $plan['id'])}}" method="POST">
+                        @csrf
+
+                        <button type="submit" title="delete" style="border: none; background-color:transparent;">
+                            <i class="fa fa-trash fa-lg text-danger"></i>
+                        </button>
+                    </form></td>
+                    @endforeach
+                    </tbody>
+                </table>
                 <div class="card-body">
-                    <a href="{{url('/fitnessplan/create/')}}" class="btn btn-primary">Opret ny program</a>
+                    <a href="{{url('/fitnessplan/create/')}}" class="btn btn-primary">Create new program</a>
 
                 </div>
             </div>
